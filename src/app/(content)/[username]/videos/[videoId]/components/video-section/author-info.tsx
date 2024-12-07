79f734efc550af48cac8e787b52154ee46cda40f
Le Minh Kha <lmkha0201@gmail.com>
2024-12-07 10:37:35 +0700

    add click action for author section in VideoDetail page

--- src/app/(content)/[username]/videos/[videoId]/components/video-section/author-info.tsx
+++ src/app/(content)/[username]/videos/[videoId]/components/video-section/author-info.tsx
@@ -1,11 +1,16 @@
+'use client';
+
 import { formatNumberToShortText } from "@/core/logic/convert";
 import { Avatar, Box, Stack, Typography } from "@mui/material";
 import { CldImage } from "next-cloudinary";
+import { useRouter } from "next/navigation";
 
 interface AuthorInfoProps {
     user: any;
 }
 export default function AuthorInfoComponent(props: AuthorInfoProps) {
+    const router = useRouter();
+
     return (
         <>
             <Stack direction={'row'} spacing={1} alignItems={'center'}>
@@ -37,8 +42,30 @@ export default function AuthorInfoComponent(props: AuthorInfoProps) {
                         }}
                     />)}
                 <Stack>
-                    <Typography variant="body1" fontWeight={'bold'}>{props.user?.username || 'username'}</Typography>
-                    <Typography variant="body2">{props.user?.fullName || 'Full Name'}</Typography>
+                    <Typography
+                        onClick={() => {
+                            props?.user?.username && router.push(`/@${props.user?.username}`);
+                        }}
+                        sx={{
+                            cursor: 'pointer',
+                        }}
+                        variant="body1"
+                        fontWeight={'bold'}
+                    >
+                        {props.user?.username || 'username'}
+                    </Typography>
+
+                    <Typography
+                        onClick={() => {
+                            props?.user?.username && router.push(`/@${props.user?.username}`);
+                        }}
+                        sx={{
+                            cursor: 'pointer',
+                        }}
+                        variant="body2"
+                    >
+                        {props.user?.fullName || 'Full Name'}
+                    </Typography>
                     <Typography variant="body2" color='textSecondary'>{props.user?.followerCount ? formatNumberToShortText(props.user?.followerCount) : '0'} followers</Typography>
                 </Stack>
             </Stack>
